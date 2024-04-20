395b5b867ce0725e2f556f71a5287688798ebd43
Helmutchan <notreal@example.com>
2024-04-20 18:16:40 +0200

    whoooops! forgot to change the content of the file..!

--- secret.py
+++ secret.py
@@ -1,1 +1,1 @@
-put all your dirty secrets here! the token that is.. and only that.. only 1 lile.. no \n or anything bad like that..!! and also make it gitignore stuff that i dont yet know..!
+key = "put your fancy key here! ♥"
